e53a2ad3d7249e06a742ef6317fe486ea636f1dc
Alan Pich <alan@orangutanstudio.com>
2013-11-26 09:16:18 +0000

    genesis

---
+++ src/AlanPich/Modx/Extras/NamespaceService.php
@@ -0,0 +1,52 @@
+<?php
+namespace AlanPich\Modx\Extras;
+
+use modX;
+
+/**
+ * Class NamespaceService
+ *
+ * Base class for defining a MODX service for a package namespace.
+ * Handles configuration and instantiation, leaving you free to
+ * focus on your custom API
+ *
+ * @package AlanPich\Modx\Extras
+ */
+abstract class NamespaceService
+{
+    /** @var modX  */
+    public $modx;
+
+
+    /**
+     * Constructor
+     *
+     * @param modX  $modx
+     * @param array $config
+     */
+    public function __construct(modX $modx, $config = array())
+    {
+        $this->modx = $modx;
+        $this->config = new NamespaceServiceConfiguration($modx,$this->getDefaultConfig());
+    }
+
+    /**
+     * Default configuration properties for this service. All properties can be
+     * overridden using MODX system settings.
+     *
+     * When retrieving settings from this service (using property name $key),
+     * the following paths are checked:
+     *
+     *  1) Look for a modx system setting called {$namespace}.{$key}
+     *  2) Check service config default values
+     *
+     * @return array
+     */
+    protected function getDefaultConfig()
+    {
+        return array();
+    }
+
+
+
+} 
---
+++ src/AlanPich/Modx/Extras/NamespaceServiceConfiguration.php
@@ -0,0 +1,175 @@
+<?php
+namespace AlanPich\Modx\Extras;
+
+/**
+ * Class NamespaceServiceConfiguration
+ *
+ * Defines a configuration object capable of being used
+ * if many different ways. The main point is to be a key-value
+ * store for config option that allows overriding of default
+ * values using modx system settings.
+ *
+ * You can get/set in several different ways...
+ *
+ * 1) As an array:
+ *      $value = $conf['myKey'];
+ *      $conf['myKey'] = 'newValue';
+ * 2) As an invokable method
+ *      $value = $conf('myKey');
+ *      $conf('myKey','newValue');
+ * 3) As an object
+ *      $value = $conf->myKey;
+ *      $conf->myKey = 'newValue';
+ *
+ * @package AlanPich\Modx\Extras
+ */
+class NamespaceServiceConfiguration implements \ArrayAccess
+{
+    /** @var  \modX */
+    protected $modx;
+    /** @var array */
+    protected $conf = array();
+
+
+    /**
+     * Constructor
+     *
+     * @param \modX $modx
+     * @param array $defaults
+     */
+    public function __construct(\modX $modx, $defaults = array())
+    {
+        $this->modx = $modx;
+        $this->conf = $defaults;
+    }
+
+    /**
+     * Get configuration as json object
+     *
+     * @return string
+     */
+    public function toJSON()
+    {
+        $conf = new \stdClass;
+        foreach ($this->conf as $key => $val) {
+            $conf->$key = $this->offsetGet($key);
+        }
+        return json_encode($conf);
+    }
+
+    /**
+     * Hydrate config params from a json-encoded string
+     *
+     * @param string $json
+     */
+    public function fromJSON($json)
+    {
+        $this->conf = (array)json_decode($json);
+    }
+
+    /**
+     * Whether a offset exists
+     *
+     * @param mixed $offset  An offset to check for
+     * @return boolean true  on success or false on failure.
+     *                       The return value will be casted to boolean if non-boolean was returned.
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->conf[$offset]);
+    }
+
+    /**
+     * Offset to retrieve
+     *
+     * @param mixed $offset The offset to retrieve
+     * @return mixed Can return all value types
+     */
+    public function offsetGet($offset)
+    {
+        // First, check for a systemSetting override
+        $sysName = 'tvimageplus.' . $offset;
+        $opt = $this->modx->getOption($sysName, null, null);
+        if (!is_null($opt)) {
+            return $opt;
+        }
+
+        if (!isset($this->conf[$offset])) {
+            return null;
+        }
+
+        return $this->conf[$offset];
+    }
+
+    /**
+     * Offset to set
+     *
+     * @param mixed $offset The offset to assign the value to
+     * @param mixed $value  The value to set
+     * @throws \Exception
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        throw new \Exception("Method NamespaceServiceConfiguration::offsetSet has not been implemented");
+    }
+
+    /**
+     * Offset to unset
+     *
+     * @param mixed $offset The offset to unset</p>
+     * @throws \Exception
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        throw new \Exception("Method NamespaceServiceConfiguration::offsetUnset has not been implemented");
+    }
+
+    /**
+     * Allow invoking the object for get/set as well
+     *
+     * @param string     $key Config property key
+     * @param null|mixed $val If set, use this method as a setter
+     * @return void|mixed
+     */
+    public function __invoke($key, $val = null)
+    {
+        if (is_null($val)) {
+            return $this->offsetGet($key);
+        } else {
+            $this->offsetSet($key, $val);
+        }
+        return null;
+    }
+
+    /**
+     * Allow access as a basic object
+     *
+     * @param string $key
+     * @param mixed  $value
+     */
+    public function __set($key, $value)
+    {
+        $this->offsetSet($key, $value);
+    }
+
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        return $this->offsetGet($key);
+    }
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function __isset($key)
+    {
+        $v = $this->offsetGet($key);
+        return $v !== null;
+    }
+}
